e4ad7192190d93ed47d99af2f39d0c636f5e2a8f
Emilio Scott <milii76@outlook.com>
2026-04-17 09:12:59 -0700

    add student class

--- src/task_3/Student.java
+++ src/task_3/Student.java
@@ -4,41 +4,41 @@ import java.util.HashMap;
 
 public class Student {
     static int totalNumOfStudents = 0;
-    String name; 
+    String name;
     HashMap<String, Double> courseAndGrades = new HashMap<>();
 
     Student(){totalNumOfStudents++;}
 
     Student(String stName){
-        this.name = stName; 
+        this.name = stName;
     }
 
-    Student (String stName, HashMap<String, Double> courseAndGrades){
-        this.name = stName; 
+    Student(String stName, HashMap<String, Double> courseAndGrades) {
+        this.name = stName;
         this.courseAndGrades = courseAndGrades;
     }
 
-    void setName(String studentName){
+    void setName(String studentName) {
         this.name = studentName;
     }
 
-    void setCourseAndGrades (HashMap<String, Double> coursesAndGrades){
+    void setCourseAndGrades(HashMap<String, Double> coursesAndGrades) {
         this.courseAndGrades = coursesAndGrades;
     }
 
-    void totalNumberofStudents (int totalNumberofStudents) {
+    void totalNumberofStudents(int totalNumberofStudents) {
         this.totalNumOfStudents = totalNumberofStudents;
     }
 
-    String getName(){
+    String getName() {
         return this.name;
     }
 
-    HashMap<String, Double> getCoursesandGrades (){
+    HashMap<String, Double> getCoursesandGrades() {
         return this.courseAndGrades;
     }
 
-    int getTotalNumberOfStudents (){
+    int getTotalNumberOfStudents() {
         return totalNumOfStudents;
     }
 
@@ -46,8 +46,8 @@ public class Student {
         System.out.println(this.courseAndGrades.toString());
 
         String[] setOfKeys = new String[this.courseAndGrades.keySet().size()];
-        for (String key : setOfKeys){
+        for (String key : setOfKeys) {
             System.out.printf("%s, %f\n", key, this.courseAndGrades.get(key));
         }
-     }
+    }
 }
